75e05160e4a0aae3f6dfc32a522b85a8c8f9468e
cornejo <cornejo.castro94@gmail.com>
2020-07-08 22:03:45 -0600

    Ejercicio 3 varialbles

---
+++ EJERCICIOS/Ejercicio 3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int numeroEntero;
+	
+	numeroEntero = 15;
+	
+	cout << "numero entero:"<< numeroEntero << endl;
+	
+	numeroEntero = 18;
+	
+	cout << "entero modificado:" <<numeroEntero;
+	return 0;
+}
