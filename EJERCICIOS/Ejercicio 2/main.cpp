e6ce2e3b52d27bc8de5970e1374ea004486c242d
cornejo <cornejo.castro94@gmail.com>
2020-07-08 21:13:58 -0600

    ejerecicio nombre

---
+++ EJERCICIOS/Ejercicio 2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	string nombre;
+	
+	cout << "ingrese nombre:";
+	cin  >> nombre;
+	 
+	cout << endl;
+	cout << "su nombre es:" << nombre;
+	cout << endl;
+	
+	 
+	return 0;
+}
