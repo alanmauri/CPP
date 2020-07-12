f5f60541839506f1e6a7a08c55a6fe3165b7d66c
cornejo <cornejo.castro94@gmail.com>
2020-07-11 21:53:52 -0600

    ejercicio lenguaje 1

--- EJERCICIOS/Ejercicio 6/main.cpp
+++ EJERCICIOS/Ejercicio 6/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	
+<<<<<<< HEAD
 	int a=0, b=0, suma=0, resta=0,multi=0,division=0;
 	
 	cout << "Ingrese el Valor de a: ";
@@ -26,5 +27,24 @@ int main(int argc, char** argv) {
 	cout << "El resultado de la multi es: " << multi<< endl;
 	cout << "El resultado de la division es: " << division<< endl;
 	
+=======
+	int a=0, b=0, suma=0, resta=0, multi=0, division=0;
+	
+	cout << "Ingrese el Valor de a : ";
+	cin>> a; 
+	
+	cout << "Ingrese el Valor de b : ";
+	cin>> b;
+	
+	suma = a + b;
+	resta = a - b;
+	multi = a * b;
+	division = a / b;
+	
+	cout << "la respuesta de la suma es: " << suma << endl;
+	cout << "la respuesta de la resta es: " << resta << endl;
+	cout << "la respuesta de la multiplicacion es: " << multi << endl;
+	cout << "la respuesta de la divicion es: " << division << endl;
+>>>>>>> 5b75012c34c7a152c91b205149e03d483e86e9a8
 	return 0;
 }
