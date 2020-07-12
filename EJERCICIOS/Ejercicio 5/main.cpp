0413cb29d079816cb3f448babd2dc62008b4714d
cornejo <cornejo.castro94@gmail.com>
2020-07-11 21:19:25 -0600

    programa lenguahe 1

---
+++ EJERCICIOS/Ejercicio 5/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int a=0, b=0, resultado=0;
+	
+	cout << "Ingrese el Valor de a: ";
+	cin >> a;
+	
+	cout << endl;
+	
+		cout << "Ingrese el Valor de b: ";
+	cin >> b;
+	
+	resultado=a+b;
+	
+	cout << "El resultado es: " << resultado<< endl;
+	
+	return 0;
+}
