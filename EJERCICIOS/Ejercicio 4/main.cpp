029509b5d0b997f1bb1d3ea806436843cda738d5
cornejo <cornejo.castro94@gmail.com>
2020-07-11 20:30:22 -0600

    Todas las variables

---
+++ EJERCICIOS/Ejercicio 4/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+bool valorBoolean;
+valorBoolean = true;
+
+int valorEntero =15;
+double valorDouble = 20.99;
+string valorString = "hola como esta";
+char valorChar = 'b';
+
+cout << "Valor Boolean: "<< valorBoolean << endl;
+cout << "Valor Double: "<< valorDouble << endl;
+cout << "Valor Entero: "<< valorEntero << endl;
+cout << "Valor String: "<< valorString << endl;
+cout << "Valor Char: "<< valorChar << endl;
+
+
+	return 0;
+}
