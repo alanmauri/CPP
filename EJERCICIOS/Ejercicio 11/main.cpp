b408722f36801a64ca3931de444c74d3c420114c
cornejo <cornejo.castro94@gmail.com>
2020-07-12 18:44:08 -0600

    Operador logico and

---
+++ EJERCICIOS/Ejercicio 11/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int a=50, b=10, c=7;
+	
+	if(a>b && a>c)
+	{
+		cout<<"A es el mayor  "<< a;
+	}
+	else if (b>a && b>c)
+	{
+		cout<<"B es el mayor  "<< b;
+	}
+	else
+	 {
+		cout<<"C es el mayor  " << c;
+	}
+	return 0;
+}
