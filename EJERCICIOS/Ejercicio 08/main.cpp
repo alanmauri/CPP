926e26d84fa9f82eee0a86bf2f28a47099b16830
cornejo <cornejo.castro94@gmail.com>
2020-07-15 16:41:17 -0600

    Factura con descuento

--- EJERCICIOS/Ejercicio 08/main.cpp
+++ EJERCICIOS/Ejercicio 08/main.cpp
@@ -17,6 +17,7 @@ int main(int argc, char** argv) {
 	cin >> descuento;
 	
 	calculodescuento=(subtotal*descuento)/100;
+	
 	calculoimpuesto=(subtotal-calculodescuento)*impuesto;
 	
 	total = subtotal-calculodescuento+calculoimpuesto;
