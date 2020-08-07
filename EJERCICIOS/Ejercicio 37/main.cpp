d260846952ff42d555deccd8c86db85789e445f9
cornejo <cornejo.castro94@gmail.com>
2020-08-06 20:12:34 -0600

    Repaso 3.

---
+++ EJERCICIOS/Ejercicio 37/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int a=0, b=10, c=4;
+	
+//	if(a>b && a>c){
+		
+//		cout<<"a mayor que b o mayor que c"
+//	}
+	
+//	while(a > b){
+//		a--;
+		
+//		if(a==20){
+//			break;
+//		}
+//	}
+	
+	while(true)
+	{
+		while(true)
+		{
+			if(b==10)
+			{
+				break;
+			}
+			b++
+		}
+		
+		if(a==1)
+		{
+			break;
+		}
+		a++;
+	}
+	
+	return 0;
+}
