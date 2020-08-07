56fdfc8381a9c091ccbbf8944008b473b1d081e7
cornejo <cornejo.castro94@gmail.com>
2020-08-06 23:56:33 -0600

    Punto de venta 2.

---
+++ EJERCICIOS/Ejercicio 41/productos.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+
+void productos(int opcion)
+{	 
+
+system ("cls");
+
+	switch (opcion)
+	{
+		
+		case 1:
+			{
+			cout<<"Bebidas calientes: "<<endl;
+			cout<<"****************** "<<endl;
+			system ("pause"); 
+			break;
+			}
+			
+		case 2:
+			{
+			cout<<"Bebidas frias: "<<endl;
+			cout<<"***************"<<endl;
+			system ("pause") ;
+			break;
+			}
+		
+		case 3:
+			{
+			cout<<"Reposteria: "<<endl;
+			cout<<"***********"<<endl;
+			system ("pause"); 
+			break;
+			}
+			
+		default:
+	 break;	
+	}
+	
+	
+}
