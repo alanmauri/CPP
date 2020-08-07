9ce6b014787afc2fbb3d843df866a4dd5866ed0f
cornejo <cornejo.castro94@gmail.com>
2020-08-06 19:42:16 -0600

    Alcance de las variables.

---
+++ EJERCICIOS/Ejercicio 36/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int numero=0;
+int sumar(int a, int b);
+
+int main(int argc, char** argv) {
+	
+	numero =5;
+	
+	cout<< sumar(5,7);
+	cout<< numero;
+	
+	cout<<endl;
+	return 0;
+}
+
+int sumar(int a, int b)
+{
+	cout<<endl;
+	numero=7;
+	
+	return a+b;
+}
