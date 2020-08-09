921c1d78d3acf27abff905f6dada2d1cb451af92
cornejo <cornejo.castro94@gmail.com>
2020-08-09 02:20:02 -0600

    Uso de libreria math.

--- EJERCICIOS/Ejercicio 51/main.cpp
+++ EJERCICIOS/Ejercicio 51/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <math.h>
-#define pi 3.1416
+
 
 using namespace std;
 
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
 	
 	double numero=0, seno=0, coseno=0, tangente=0;
 	
-	numero = 2*pi;
+
 	seno = sin(numero);
 	coseno= cos(numero);
 	tangente= tan(numero);
